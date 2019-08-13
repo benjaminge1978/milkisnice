8a58ec9bb4f9440bfd30b1bf7d81d06a126b4857
Ben Davidson <ben@milkisnice.com>
2019-08-13 21:38:05 +0200

    First commit

---
+++ src/pages/about.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Header from "../components/header"
+export default () => (
+    <div stlye={{ color: 'teal'}}>
+        <Header headerText="About Gatsby" />
+        <Header headerText="It's pretty cool"></Header>
+        <p>Wow such power. Very React</p>
+    </div>
+)
